3111106e968042b8bf6ad22a15136177c03815b9
Darkerst <darkerst@gmail.com>
2022-03-07 02:59:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nodejs-x3sute
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nodejs-x3sute)
---
+++ index.js
@@ -0,0 +1,11 @@
+import { forEach } from 'async';
+
+forEach(
+  [],
+  (item, cb) => {
+    cb('1');
+  },
+  (err, result) => {
+    console.log(err);
+  }
+);
